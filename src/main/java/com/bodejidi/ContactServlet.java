daac4281a22f2888f81f1367864fed77e2c1ec6a
lidgik <dbwen123@gmail.com>
2014-07-25 22:10:43 +0800

    添加显示所有联系人的代码

--- src/main/java/com/bodejidi/ContactServlet.java
+++ src/main/java/com/bodejidi/ContactServlet.java
@@ -22,6 +22,56 @@ public class ContactServlet extends HttpServlet {
 
 		if((request.getParameter("id") == null) || (request.getParameter("id") == "")) {
 			response.getWriter().println("ContactList");
+
+			try {
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+			} catch (Exception e) {
+				// handle the error
+			}
+
+			try {
+				connection = DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&password=");
+				statement = connection.createStatement();
+				resultSet = statement.executeQuery("select * from contact");
+
+				while(resultSet.next()) {
+					response.getWriter().println("Following is the information of:" + resultSet.getString("name"));
+					response.getWriter().println("Name:" + resultSet.getString("name"));
+					response.getWriter().println("Mobile:" + resultSet.getString("mobile"));
+					response.getWriter().println("Vpmn:" + resultSet.getString("vpmn"));
+					response.getWriter().println("Email:" + resultSet.getString("email"));
+					response.getWriter().println("HomeAddress:" + resultSet.getString("home_address"));
+					response.getWriter().println("OfficeAddress:" + resultSet.getString("office_address"));
+					response.getWriter().println();
+				}
+			} catch (SQLException sqle) {
+				response.getWriter().println("cannot connect to db");
+				sqle.printStackTrace();
+			}
+
+			if(resultSet != null) {
+				try {
+					resultSet.close();
+				} catch (Exception e) {
+
+				}
+			}
+
+			if(statement != null) {
+				try {
+					statement.close();
+				} catch (Exception e) {
+
+				}
+			}
+			
+			if(connection != null) {
+				try {
+					connection.close();
+				} catch (Exception e) {
+
+				}
+			}
 		}
 		else {
 			response.getWriter().println("ContactShowById");
